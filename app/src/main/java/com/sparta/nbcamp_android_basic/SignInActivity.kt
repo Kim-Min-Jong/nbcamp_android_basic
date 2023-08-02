3519ded48f6d7632ae171a5513ffe94cb4c1554f
minjong kim <alswhddkdl@gmail.com>
2023-08-02 20:46:25 +0900

    feat: 선택과제 1 구현

--- app/src/main/java/com/sparta/nbcamp_android_basic/SignInActivity.kt
+++ app/src/main/java/com/sparta/nbcamp_android_basic/SignInActivity.kt
@@ -1,10 +1,12 @@
 package com.sparta.nbcamp_android_basic
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.widget.doOnTextChanged
 import com.sparta.nbcamp_android_basic.util.toast
 
@@ -15,7 +17,15 @@ class SignInActivity : AppCompatActivity() {
     private val btnSignIn by lazy { findViewById<Button>(R.id.buttonSignIn) }
     private lateinit var id: String
     private lateinit var pwd: String
-
+    private val activityLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if(it.resultCode == Activity.RESULT_OK) {
+                val id = it.data?.getStringExtra("ID")
+                val pwd = it.data?.getStringExtra("PWD")
+                editTextId.setText(id)
+                editTextPwd.setText(pwd)
+            }
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
@@ -34,7 +44,7 @@ class SignInActivity : AppCompatActivity() {
             toast("로그인 성공")
         }
         btnSignIn.setOnClickListener {
-            startActivity(Intent(this, SignUpActivity::class.java))
+            activityLauncher.launch(Intent(this, SignUpActivity::class.java))
         }
     }
     private fun initEditText() {
--- app/src/main/java/com/sparta/nbcamp_android_basic/SignUpActivity.kt
+++ app/src/main/java/com/sparta/nbcamp_android_basic/SignUpActivity.kt
@@ -1,9 +1,13 @@
 package com.sparta.nbcamp_android_basic
 
+import android.app.Activity
+import android.app.Instrumentation.ActivityResult
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.widget.doOnTextChanged
 import com.sparta.nbcamp_android_basic.util.toast
 
@@ -15,6 +19,7 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var name: String
     private lateinit var id: String
     private lateinit var pwd: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -28,6 +33,11 @@ class SignUpActivity : AppCompatActivity() {
                 toast("입력되지 않은 정보가 있습니다.")
                 return@setOnClickListener
             }
+            val intent = Intent(this, SignInActivity::class.java).apply {
+                putExtra("ID", id)
+                putExtra("PWD",pwd)
+            }
+            setResult(RESULT_OK, intent)
             finish()
         }
     }

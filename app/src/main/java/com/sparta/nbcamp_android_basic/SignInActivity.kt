ba50f86158897176098dbd55efe270b75752bc53
minjong kim <alswhddkdl@gmail.com>
2023-08-04 11:31:05 +0900

    feat: Sign in - TextInputLayout 적용

--- app/src/main/java/com/sparta/nbcamp_android_basic/SignInActivity.kt
+++ app/src/main/java/com/sparta/nbcamp_android_basic/SignInActivity.kt
@@ -8,13 +8,18 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.widget.doOnTextChanged
+import com.google.android.material.textfield.TextInputLayout
+import com.sparta.nbcamp_android_basic.util.shake
 import com.sparta.nbcamp_android_basic.util.toast
+import com.sparta.nbcamp_android_basic.util.validate
 
 class SignInActivity : AppCompatActivity() {
     private val editTextId by lazy { findViewById<EditText>(R.id.editTextId) }
     private val editTextPwd by lazy { findViewById<EditText>(R.id.editTextTextPassword) }
     private val btnLogIn by lazy { findViewById<Button>(R.id.buttonLogIn) }
     private val btnSignIn by lazy { findViewById<Button>(R.id.buttonSignIn) }
+    private val textInputId by lazy { findViewById<TextInputLayout>(R.id.textInputLayoutId)}
+    private val textInputPwd by lazy { findViewById<TextInputLayout>(R.id.textInputLayoutPwd)}
     private lateinit var id: String
     private lateinit var pwd: String
     private val activityLauncher =
@@ -34,14 +39,41 @@ class SignInActivity : AppCompatActivity() {
     }
     private fun initButton() {
         btnLogIn.setOnClickListener {
-            if(::id.isInitialized.not() || ::pwd.isInitialized.not() || pwd == "" || id == "") {
-                toast("아이디/비밀번호를 확인해주세요")
+            if((::id.isInitialized.not() && ::pwd.isInitialized.not()) || (id == "" && pwd == "")) {
+                toast(getString(R.string.check_id_pwd))
+                validate(textInputId, getString(R.string.do_not_input_id))
+                validate(textInputPwd, getString(R.string.do_not_input_pwd))
+                shake(editTextId, this)
+                shake(editTextPwd, this)
+                return@setOnClickListener
+            }
+            else if((::id.isInitialized.not() && ::pwd.isInitialized) || id == "") {
+                toast(getString(R.string.check_id))
+                validate(textInputId,getString(R.string.do_not_input_id))
+                textInputPwd.apply {
+                    error = null
+                    isErrorEnabled = false
+                }
+                shake(editTextId, this)
+                return@setOnClickListener
+            }
+            else if((::id.isInitialized && ::pwd.isInitialized.not()) || pwd == ""){
+                toast(getString(R.string.check_pwd))
+                editTextId.error = null
+                validate(textInputPwd, getString(R.string.do_not_input_pwd))
+                textInputId.apply {
+                    error = null
+                    isErrorEnabled = false
+                }
+                shake(editTextPwd, this)
                 return@setOnClickListener
             }
             startActivity(Intent(this, HomeActivity::class.java).apply {
                 putExtra("ID", id)
+                editTextId.error = null
+                editTextPwd.error = null
             })
-            toast("로그인 성공")
+            toast(getString(R.string.log_in_success))
         }
         btnSignIn.setOnClickListener {
             activityLauncher.launch(Intent(this, SignUpActivity::class.java))
--- app/src/main/java/com/sparta/nbcamp_android_basic/SignUpActivity.kt
+++ app/src/main/java/com/sparta/nbcamp_android_basic/SignUpActivity.kt
@@ -1,22 +1,25 @@
 package com.sparta.nbcamp_android_basic
 
-import android.app.Activity
-import android.app.Instrumentation.ActivityResult
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.widget.doOnTextChanged
+import com.google.android.material.textfield.TextInputLayout
 import com.sparta.nbcamp_android_basic.model.User
 import com.sparta.nbcamp_android_basic.util.toast
+import com.sparta.nbcamp_android_basic.util.validate
+
 
 class SignUpActivity : AppCompatActivity() {
     private val editTextName by lazy { findViewById<EditText>(R.id.editTextName)}
     private val editTextId by lazy { findViewById<EditText>(R.id.editTextSignId)}
     private val editTextPwd by lazy { findViewById<EditText>(R.id.editTextTextSignPassword)}
     private val btnSignIn by lazy {findViewById<Button>(R.id.buttonSignLogIn)}
+    private val textInputId by lazy { findViewById<TextInputLayout>(R.id.textInputLayoutId)}
+    private val textInputPwd by lazy { findViewById<TextInputLayout>(R.id.textInputLayoutPwd)}
+    private val textInputName by lazy { findViewById<TextInputLayout>(R.id.textInputLayoutName)}
     private lateinit var name: String
     private lateinit var id: String
     private lateinit var pwd: String
@@ -31,11 +34,11 @@ class SignUpActivity : AppCompatActivity() {
     private fun initButton() {
         btnSignIn.setOnClickListener {
             if(::name.isInitialized.not() || ::id.isInitialized.not() || ::pwd.isInitialized.not()) {
-                toast("입력되지 않은 정보가 있습니다.")
+                toast(getString(R.string.empty_info_exist))
                 return@setOnClickListener
             }
             if(list.count{ it.id == id } >= 1) {
-                toast("중복된 아이디가 있습니다.")
+                toast(getString(R.string.duplicated_id))
                 return@setOnClickListener
             }
             val intent = Intent(this, SignInActivity::class.java).apply {
